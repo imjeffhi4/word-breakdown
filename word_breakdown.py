cf59c8ad71809c255b5269ef0a9ef0fb6951cea0
imjeffhi4 <60154931+imjeffhi4@users.noreply.github.com>
2022-07-31 12:20:00 -0500

    Update word_breakdown.py

--- word_breakdown.py
+++ word_breakdown.py
@@ -11,7 +11,7 @@ device = 0 if torch.cuda.is_available() else -1
 morph_path = './Model'
 morph_tokenizer = AutoTokenizer.from_pretrained(morph_path)
 morph_model = AutoModelForCausalLM.from_pretrained(morph_path).to(device)
-breakdown_pipe = pipeline('text-generation', model=morph_model, tokenizer=morph_tokenizer, pipeline_class=WordBreakdownPipeline, device=0)
+breakdown_pipe = pipeline('text-generation', model=morph_model, tokenizer=morph_tokenizer, pipeline_class=WordBreakdownPipeline, device=device)
 
 class UserInput(BaseModel):
     word: str
